78636c089fa30f42e06858d4a90b493c720c0cef
Ivan Herlan Herbas Zubieta <79385821+ivancidev@users.noreply.github.com>
2024-11-12 08:13:56 -0400

    feat: add new icon components for brook marks, home, lists, message, notification, and search

---
+++ src/common/icons/index.ts
@@ -0,0 +1,7 @@
+export * from './brook-marks-icons';
+export * from './home-icon';
+export * from './index';
+export * from './lists-icon';
+export * from './message-icon';
+export * from './notification-icon';
+export * from './search-icon';
