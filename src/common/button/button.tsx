7a2fc1e3fd3fcc37665f00f60255124212c50622
Ivan Herlan Herbas Zubieta <79385821+ivancidev@users.noreply.github.com>
2024-12-30 18:00:46 -0400

    feat: enhance Button component with disabled state and variant support for improved usability

--- src/common/button/button.tsx
+++ src/common/button/button.tsx
@@ -1,12 +1,23 @@
 interface ButtonProps {
   label: string;
   onClick: () => void;
+  isDesabled?: boolean;
+  variant?: 'primary' | 'secondary';
 }
-export const Button = ({ label, onClick }: ButtonProps) => {
+export const Button = ({
+  label,
+  onClick,
+  isDesabled = false,
+  variant = 'primary',
+}: ButtonProps) => {
+  const variantClasses = variant === 'primary' ? 'px-20 py-2' : 'px-6 py-1';
   return (
     <button
       onClick={onClick}
-      className="ml-4 mt-2 px-20 py-2 font-semibold bg-slate-50 rounded-3xl ed- hover:bg-slate-200 text-black"
+      disabled={isDesabled}
+      className={`ml-4 mt-2 ${variantClasses} font-semibold bg-slate-50 rounded-3xl hover:bg-slate-200 text-black
+      ${isDesabled ? 'cursor-not-allowed opacity-50' : 'cursor-pointer'}
+      `}
     >
       {label}
     </button>
